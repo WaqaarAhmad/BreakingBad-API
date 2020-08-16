0e12e86de2d8f15eb0aece12ea5511c6f428937e
Waqar Ahmad <waqaar.ahmad@yahoo.com>
2020-08-16 14:58:01 +0500

    Build 1.1

---
+++ build/precache-manifest.d415305f64769edaf19b1a342ffbe4e6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8e2d94100d7ad3f61d2648611286a3d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cae52370386c9ee834f8",
+    "url": "/static/css/main.aceea456.chunk.css"
+  },
+  {
+    "revision": "a871687e8b0ee59e3956",
+    "url": "/static/js/2.18c36af0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.18c36af0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cae52370386c9ee834f8",
+    "url": "/static/js/main.a944e5bc.chunk.js"
+  },
+  {
+    "revision": "dab11903091cda04344c",
+    "url": "/static/js/runtime-main.71f22666.js"
+  },
+  {
+    "revision": "1be2d3a0dc2064fe215cebb23799d503",
+    "url": "/static/media/bg.1be2d3a0.jpg"
+  },
+  {
+    "revision": "3795f66d4a869a7faeb4b425446daf33",
+    "url": "/static/media/logo.3795f66d.png"
+  },
+  {
+    "revision": "235b9b3ed13f64542cb0792e6ae7b5c4",
+    "url": "/static/media/spinner.235b9b3e.gif"
+  }
+]);
